72566c0ed6022a260c0e2de041acedc847d334bb
csucc-cloud <sialongodavie68@gmail.com>
2026-05-05 15:43:21 +0800

    Update page.tsx

--- app/(student)/attendance/page.tsx
+++ app/(student)/attendance/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { createClient } from '@/utils/supabase/client'
+import { createClient } from '../../../utils/supabase/client'
 import { CheckCircle, Clock } from 'lucide-react'
 
 export default function StudentAttendance() {
